4f0786d5460962b1d73275475069aaef18dbe914
unknown <up202108724@g.uporto.pt>
2024-03-11 18:21:33 +0000

    Repeated some vertices only

--- tp3/MyTriangleBig.js
+++ tp3/MyTriangleBig.js
@@ -18,7 +18,10 @@ export class MyTriangleBig extends CGFobject {
         this.vertices = [
             -2, 0, 0,    //0
             2, 0, 0,	//1
-            0, 2, 0,	//2
+            0, 2, 0,    //2
+            -2, 0, 0,    //3
+            2, 0, 0,	//4
+            0, 2, 0,	//5
         ];
 
         //Counter-clockwise reference of vertices
