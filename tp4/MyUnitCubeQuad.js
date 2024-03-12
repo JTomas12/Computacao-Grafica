026f5dc6c0cea15634f8b7c157f7e5cac5e08482
unknown <up202108724@g.uporto.pt>
2024-03-12 11:20:53 +0000

    Ready to start tp4

---
+++ tp4/MyUnitCubeQuad.js
@@ -0,0 +1,57 @@
+import { CGFobject } from '../lib/CGF.js';
+import { MyQuad } from './MyQuad.js';
+
+export class MyUnitCubeQuad extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.initQuads();
+    }
+
+    initQuads() {
+        this.quad = new MyQuad(this.scene);
+    }
+
+    display() {
+
+        // Face frontal
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        // Face traseira
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI, 0, 1, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        // Face lateral direita
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        // Face lateral esquerda
+        this.scene.pushMatrix();
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        // Face superior
+        this.scene.pushMatrix();
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        // Face inferior
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+    }
+}
