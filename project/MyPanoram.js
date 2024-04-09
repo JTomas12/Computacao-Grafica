879fe1849f4d38ee6c08e9bca1c7df91d552ce6c
up202108738 <up202108738@up.pt>
2024-04-09 12:12:37 +0100

    Panoram class

---
+++ project/MyPanoram.js
@@ -0,0 +1,32 @@
+import {CGFobject} from '../lib/CGF.js';
+import { MySphere } from '../project/MySphere.js';
+/**
+ * MyDiamond
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyPanoram extends CGFobject {
+    constructor(scene, texture) {
+        super(scene);
+        this.sphere = new MySphere(scene, 20, 20, 1, 1);
+        this.material = new CGFappearance(this.scene)
+        this.material.setEmission(1, 1, 1, 1)
+        this.material.setTexture(texture)
+        
+    }
+		
+    
+    display() {
+        this.scene.pushMatrix();
+        this.material.apply();
+        this.sphere.display();
+        this.scene.popMatrix();
+    
+    }
+    /*
+    updateTexCoords(coords) {
+		this.texCoords = [...coords];
+		this.updateTexCoordsGLBuffers();
+	}
+    */
+}
