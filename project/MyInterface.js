2d2cfc8c2ad3319baaae310b304e81b94e6db7a4
AndreSilva <up202108724@g.uporto.pt>
2024-05-21 01:32:00 +0100

    Finished Commenting Code

--- project/MyInterface.js
+++ project/MyInterface.js
@@ -18,49 +18,49 @@ export class MyInterface extends CGFinterface {
         this.gui = new dat.GUI();
 
         //Checkbox element in GUI
-        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis'); // Adds a checkbox to activate/deactivate the axis
 
         //Slider element in GUI
-        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor'); // Adds a slider to change the scale of the object
         
-        this.gui.add(this.scene, 'beescaleFactor',0.5, 3).name('Bee Scale Factor')
-        this.gui.add(this.scene, 'speedFactor', 0.1, 3).name('Speed Factor');
+        this.gui.add(this.scene, 'beescaleFactor',0.5, 3).name('Bee Scale Factor') // Adds a slider to change the scale of the bee (not used)
+        this.gui.add(this.scene, 'speedFactor', 0.1, 3).name('Speed Factor'); // Adds a slider to change the speed of the bee movements (more noticiably the rotation of the bee)
 
-        this.gui.add(this.scene, 'displayGarden').name('Display Garden');
+        this.gui.add(this.scene, 'displayGarden').name('Display Garden'); // Adds a checkbox to activate/deactivate the garden visibility
 
-        this.gui.add(this.scene, 'displaySphere').name('Display Sphere');
-        this.gui.add(this.scene, 'displayPanorama').name('Display Panorama');
-        this.gui.add(this.scene, 'displayRockSet').name('Display Rock Set');
-        this.gui.add(this.scene, 'displayHive').name('Display Hive');
+        this.gui.add(this.scene, 'displaySphere').name('Display Sphere'); // Adds a checkbox to activate/deactivate the sphere visibility (not used to the final scene)
+        this.gui.add(this.scene, 'displayPanorama').name('Display Panorama'); // Adds a checkbox to activate/deactivate the panorama visibility (not used to the final scene)
+        this.gui.add(this.scene, 'displayRockSet').name('Display Rock Set');// Adds a checkbox to activate/deactivate the rock set visibility (not used to the final scene)
+        this.gui.add(this.scene, 'displayHive').name('Display Hive'); // Adds a checkbox to activate/deactivate the hive visibility
 
         this.gui.add(this.scene, 'displayFlower').name('Display Flower');
         this.gui.add(this.scene, 'displayPetal').name('Display Petal');
 
         this.gui.add(this.scene, 'displayBee').name('Display Bee');
-        this.gui.add(this.scene, 'displayTerrain').name('Display Terrain');
-        this.gui.add(this.scene, 'showShaderCode').name('Show Shader Code').onChange(this.scene.onShaderCodeVizChanged.bind(this.scene));
+        this.gui.add(this.scene, 'displayTerrain').name('Display Terrain'); // Adds a checkbox to activate/deactivate the terrain visibility
+        this.gui.add(this.scene, 'showShaderCode').name('Show Shader Code').onChange(this.scene.onShaderCodeVizChanged.bind(this.scene)); //Shows the shader code that is being used both for fragment shader and the vertex shader
         const beeFolder = this.gui.addFolder('Bee Settings');
-        beeFolder.add(this.scene, 'decelerationRate', 0.1, 1).name('Deceleration Rate');
-        beeFolder.add(this.scene, 'accelerationRate', 0.1, 1).name('Acceleration Rate');
-        beeFolder.add(this.scene, 'beemaxSpeed', 0.1, 5).name('Bee Max Speed');
-        const gardenFolder = this.gui.addFolder('Garden Settings');
+        beeFolder.add(this.scene, 'decelerationRate', 0.1, 1).name('Deceleration Rate'); // Adds a slider to change the deceleration rate of the bee, that is, how fast the bee stops
+        beeFolder.add(this.scene, 'accelerationRate', 0.1, 1).name('Acceleration Rate'); // Adds a slider to change the acceleration rate of the bee, that is, how fast the bee speeds up (reaches the max speed)
+        beeFolder.add(this.scene, 'beemaxSpeed', 0.1, 5).name('Bee Max Speed'); // Adds a slider to change the max speed of the bee
+        const gardenFolder = this.gui.addFolder('Garden Settings'); // Adds a folder to the GUI to group the garden settings
         gardenFolder.add(this.scene, 'gardenRows', 1, 10).step(1).name('Rows').onChange(() => {
-          this.scene.updateGarden();
+          this.scene.updateGarden(); // Adds a slider to change the number of rows in the garden and updates the garden (more especifically the visibility of garden objects in rows)
         });
         
         gardenFolder.add(this.scene, 'gardenCols', 1, 10).step(1).name('Columns').onChange(() => {
-          this.scene.updateGarden();
+          this.scene.updateGarden();// Adds a slider to change the number of rows in the garden and updates the garden (more especifically the visibility of garden objects in columns)
         });
         gardenFolder.open();
         const petalFolder = this.gui.addFolder('Petal Settings');
         petalFolder.add(this.scene, 'rotationAngle', -Math.PI, Math.PI).name('Rotation Angle').onChange(() => {
-          this.scene.updatePetal();
+          this.scene.updatePetal(); // Adds a slider to change the rotation angle of the petal and updates the petal on the main flower
         });
         petalFolder.add(this.scene, 'prismAngle', -Math.PI, Math.PI).name('Prism Angle').onChange(() => {
-          this.scene.updatePetal();
+          this.scene.updatePetal(); // Adds a slider to change the prism angle of the petal and updates the petal on the main flower
         });
         
-        this.initKeys();
+        this.initKeys(); // Initializes the keys for the interface, initiating the keyboard keys processing
         return true;
     }
     initKeys(){
@@ -69,13 +69,13 @@ export class MyInterface extends CGFinterface {
       this.activeKeys={};
     }
     processKeyDown(event){
-      this.activeKeys[event.code]=true;
+      this.activeKeys[event.code]=true; // When the key is pressed, the key is set to true
       console.log(event.code);
     }
     processKeyUp(event){
-      this.activeKeys[event.code]=false;
+      this.activeKeys[event.code]=false; // When the key is released, the key is set to false
     }
     isKeyPressed(keyCode){
-      return this.activeKeys[keyCode] || false;
+      return this.activeKeys[keyCode] || false; // Returns true if the key is pressed, false otherwise
     }
 }
--- project/MyScene.js
+++ project/MyScene.js
@@ -46,28 +46,26 @@ export class MyScene extends CGFscene {
     
   }
   pickPollen() {
-
+    //Action to pick pollen from the flower
     if (this.pollenPresentInFlower) {
-      this.pollenPresentInFlower = false;
+      this.pollenPresentInFlower = false; // Activated when Pollen is picked from the flower
       return new MyPollen(this); 
     }
     return null; 
   }
   isPollenPresent() {
-    return this.pollenPresentInFlower;
+    return this.pollenPresentInFlower; // Returns if pollen is present in the flower
   }
   dropPollen() {
-    this.pollenPresentInHive = true;
+    this.pollenPresentInHive = true; // Activated when Pollen is dropped in the hive
   }
   isPollenPresentInHive() {
-    return this.pollenPresentInHive;
+    return this.pollenPresentInHive; // Returns if pollen is present in the hive
   }
-  resetPollen() {
+  resetPollen() { // Reset the pollen state
     this.pollenPresentInFlower = true;
     this.pollenPresentInHive = false;
 
-    this.selectedExampleShader = 0;
-    this.showShaderCode = false;
   }
   checkkeys() {
     var text = "Keys pressed: ";
@@ -93,11 +91,11 @@ export class MyScene extends CGFscene {
     }
   }
   updateGarden() {
-    this.garden = new MyGarden(this, this.gardenRows, this.gardenCols);
+    this.garden = new MyGarden(this, this.gardenRows, this.gardenCols); // Create a new garden with the updated number of rows and columns
   }
   updatePetal() {
-    this.flower = new MyFlower(this,3,5,1.2,[128,0,0],0.3,[0,128,0],3,3,[0,0,128], this.rotationAngle, this.prismAngle,false, [0,255,0],5);
-    this.petal = new MyPetal(this, this.rotationAngle, this.prismAngle,this.flower.petal_color);
+    this.flower = new MyFlower(this,3,5,1.2,[128,0,0],0.3,[0,128,0],3,3,[0,0,128], this.rotationAngle, this.prismAngle,false, [0,255,0],5); // Create a new flower with the updated petal angles
+    this.petal = new MyPetal(this, this.rotationAngle, this.prismAngle,this.flower.petal_color); // Create a new petal with the updated petal angles
   } 
   init(application) {
     super.init(application);
@@ -123,29 +121,28 @@ export class MyScene extends CGFscene {
     this.rockSet4 = new MyRockSet(this, 10);
     this.speedFactor = 1;
     this.beemaxSpeed = 1;
-    this.beescaleFactor = 1;
-    this.decelerationRate = 0.5;
-    this.accelerationRate = 0.5;
-    this.rock = new MyRock(this, 16, 8);
-    this.garden = new MyGarden(this, 5, 5);
-    this.sphere = new MySphere(this, 1, 20,20,1, 1, 1);
+    this.beescaleFactor = 1; //not used
+    this.decelerationRate = 0.5; // value of the deceleration rate of the bee ( how fast the bee stops)
+    this.accelerationRate = 0.5; // value of the acceleration rate of the bee (how fast the bee speeds up)
+    this.rock = new MyRock(this, 16, 8); // Create a new rock
+    this.garden = new MyGarden(this, 5, 5); // Create a new garden with 5 rows and 5 columns
+    this.sphere = new MySphere(this, 1, 20,20,1, 1, 1); // Create a new sphere with radius 1 ( not used to the final scene)
     this.panoramTexture = new CGFtexture(this, "images/panoram2.jpg");
     this.earthTexture = new CGFtexture(this, "images/earth.jpg");
     this.panorama = new MyPanoram(this, this.panoramTexture);
-    this.leaf= new MyLeaf(this);
-    this.bee = new MyBee(this, 0, 20, 0, false);
-    this.hive = new MyHive(this, false);
-    this.pollen = new MyPollen(this);
+    this.leaf= new MyLeaf(this); // Create a new leaf (not used to the final scene)
+    this.bee = new MyBee(this, 0, 20, 0, false); // Create a new bee, and it's initial position is (0,20,0)
+    this.hive = new MyHive(this, false); // Create a new hive (with no pollen in it)
+    this.pollen = new MyPollen(this); // Create a new pollen
     this.material = new CGFappearance(this);
     this.material.setEmission(1, 1, 1, 1)
     this.material.setTexture(this.earthTexture)
     this.plane = new MyPlane(this,30);
-    this.flower = new MyFlower(this,3,5,1.2,[128,128,0],0.3,[0,128,0],3,3,[0,0,128], this.rotationAngle, this.prismAngle, false, [0,255,0],5); 
-    //this.garden = new MyGarden(this, this.speedFactor, this.speedFactor);
+    this.flower = new MyFlower(this,3,5,1.2,[128,128,0],0.3,[0,128,0],3,3,[0,0,128], this.rotationAngle, this.prismAngle, false, [0,255,0],5); // Create a new flower with the following parameters
     //(scene, outer_radius,number_of_petals, receptacle_radius, receptacle_color , stem_radius ,stem_color, stem_stacks,stem_height, petal_color) 
     //this.petal = new MyPetal(this, this.rotationAngle, this.prismAngle,this.flower.petal_color);
     //Objects connected to MyInterface
-    this.terrain = new MyTerrain(this, 50);
+    this.terrain = new MyTerrain(this, 50); // Create a new terrain with 50 width
     this.displayAxis = true;
     this.displaySphere = false;
     this.displayPanorama = true;
@@ -163,26 +160,25 @@ export class MyScene extends CGFscene {
     //this.displayPetal=true;
     this.enableTextures(true);
 
-    this.appStartTime = Date.now();
-    this.setUpdatePeriod(50);
+    this.setUpdatePeriod(50); // Set the update period to 50 ms ( calls update() every 50 ms)
 
     this.texture = new CGFtexture(this, "images/terrain.jpg");
     this.appearance = new CGFappearance(this);
     this.appearance.setTexture(this.texture);
     //this.appearance.setTextureWrap('REPEAT', 'REPEAT');
     
-    this.shadersDiv = document.getElementById("shaders");
-		this.vShaderDiv = document.getElementById("vshader");
-		this.fShaderDiv = document.getElementById("fshader");
+    this.shadersDiv = document.getElementById("shaders"); // Get the shaders div
+		this.vShaderDiv = document.getElementById("vshader"); // Get the vertex shader div
+		this.fShaderDiv = document.getElementById("fshader"); // Get the fragment shader div
 
-    this.onShaderCodeVizChanged(this.showShaderCode);
+    this.onShaderCodeVizChanged(this.showShaderCode); // Call the onShaderCodeVizChanged function with the showShaderCode value, when showShaderCode is true, the shader code is displayed
 		//this.onSelectedShaderChanged(this.selectedExampleShader);
     
-    this.starttime= Date.now();
+    this.starttime= Date.now(); // Get the start time of the application (repeated code, to be revised) 
 
-    // shaders
+    // list of extensible shaders
     this.testShaders = [
-      new CGFshader(this.gl, "shaders/grass.vert", "shaders/grass.frag"),
+      new CGFshader(this.gl, "shaders/grass.vert", "shaders/grass.frag"), // Create a new shader with the grass vertex and fragment shaders
       //new CGFshader(this.gl, "shaders/flat.vert", "shaders/flat.frag"),
     ];
     this.setUpdatePeriod(50);
@@ -203,14 +199,16 @@ export class MyScene extends CGFscene {
       vec3.fromValues(0, 0, 0)
     );
   }
+  /* never used
   setDefaultAppearance() {
     this.setAmbient(0.2, 0.4, 0.8, 1.0);
     this.setDiffuse(0.2, 0.4, 0.8, 1.0);
     this.setSpecular(0.2, 0.4, 0.8, 1.0);
     this.setShininess(10.0);
   }
-
+  */
   update(time){
+    //Update the bee position, as well as the shader update the oscilation of vertexes associated and time factor (randomOsilation is a random value between 0 and 1, but it is not used )
     let elapsed_time = (time- this.starttime)/1000.0;
     this.bee.update(elapsed_time, this.beescaleFactor ,this.speedFactor);
     this.testShaders[0].setUniformsValues({ timeFactor: time / 100 % 1000, randomOscilation: Math.random() });
@@ -222,9 +220,9 @@ export class MyScene extends CGFscene {
       console.log("shadersDiv is defined");
       if (v){
         
-        this.shadersDiv.style.display = "block";
-        this.vShaderDiv.innerHTML = "<xmp>" + getStringFromUrl(this.testShaders[0].vertexURL) + "</xmp>";
-		    this.fShaderDiv.innerHTML = "<xmp>" + getStringFromUrl(this.testShaders[0].fragmentURL) + "</xmp>";
+        this.shadersDiv.style.display = "block"; // Display the shaders div
+        this.vShaderDiv.innerHTML = "<xmp>" + getStringFromUrl(this.testShaders[0].vertexURL) + "</xmp>"; // Get the vertex shader code from the URL and display it
+		    this.fShaderDiv.innerHTML = "<xmp>" + getStringFromUrl(this.testShaders[0].fragmentURL) + "</xmp>"; // Get the fragment shader code from the URL and display it
       
         } else
         {this.shadersDiv.style.display = "none";}
@@ -232,16 +230,7 @@ export class MyScene extends CGFscene {
       console.error("shadersDiv is not defined or accessible.");
     }
 	}
-  // called when a new shader is selected, updated from the TP5 code, to be revised
-	
-
-	// called when the scale factor changes on the interface
-	onScaleFactorChanged(v) {
-		this.testShaders[this.selectedExampleShader].setUniformsValues({ normScale: this.scaleFactor });
-	}
-
-
-
+  
 
   display() {
     // ---- BEGIN Background, camera and axis setup
@@ -261,22 +250,22 @@ export class MyScene extends CGFscene {
     if (!this.displayFlower) {
       this.pollenPresentInFlower = false;
     }
-
+    //Display the sphere if checked
     if (this.displaySphere) {
      
       this.sphere.display();
 
     }
-
+    //Display the panorama if checked
     if (this.displayPanorama) {
       this.panorama.display();
     }
 
-    
+    //Display the bee if checked
     if(this.displayBee){
       this.bee.display();
     }
-
+    //Display the rock set if checked ( a composition of rocksets that is)
     if (this.displayRockSet) {
       this.rockSet.display();
       this.pushMatrix();
@@ -295,18 +284,15 @@ export class MyScene extends CGFscene {
       this.rockSet4.display();
       this.popMatrix();
     }
-
+    //Display the hive if checked
     if(this.displayHive){
       this.hive.display();
     }
-
-    if(this.displayFlower){
-      this.flower.display();
-    }
-
+    //Display the Garden if checked
     if(this.displayGarden){
       this.garden.display();
     }
+    
     if(this.pollenPresentInFlower){
       this.pushMatrix();
       this.translate(0, 3, 1.3);
@@ -314,15 +300,16 @@ export class MyScene extends CGFscene {
       this.pollen.display();
       this.popMatrix();
     }
+    
     if(this.displayTerrain){
       this.terrain.display();
       
     }
-    
-    
+    //Display the Main Flower
     if(this.displayFlower){
       this.flower.display();
     }
+    //Display the pollen if it is present in the hive
     if(this.pollenPresentInHive){
       this.pushMatrix();
       this.translate(-10.8, 4.7, 1.2);
@@ -331,7 +318,7 @@ export class MyScene extends CGFscene {
       this.pollen.display();
       this.popMatrix();
     }
-    //this.leaf.display();
+    
     // ---- BEGIN Primitive drawing section
 
     this.pushMatrix();
@@ -344,7 +331,7 @@ export class MyScene extends CGFscene {
 
     // ---- END Primitive drawing section
     // restore default shader (will be needed for drawing the axis in next frame)
-    this.setActiveShader(this.defaultShader);
+    this.setActiveShader(this.defaultShader); 
   }
 
 }
